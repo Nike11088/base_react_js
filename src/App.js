079c4c4989f14b879944536191e51c3b018f6cd9
Никита Абакумов <nabakumov@mt.1tv.ru>
2022-04-15 18:53:01 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react'
-import Counter from './components/counter'
+import React, { useState } from 'react'
+import PostList from './components/PostList'
+import MyButton from './components/UI/button/MyButton'
+import './styles/app.css'
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript', body: 'Description' },
+    { id: 2, title: 'Javascript 2', body: 'Description' },
+    { id: 3, title: 'Javascript 3', body: 'Description' },
+  ])
+
   return (
-    <div>
-      <Counter />
+    <div className="app">
+      <form>
+        <input type="text" placeholder="Название поста" />
+        <input type="text" placeholder="Описание поста" />
+        <MyButton disabled>Добавить пост</MyButton>
+      </form>
+      <PostList posts={posts} title="Посты про JS" />
     </div>
   )
 }
